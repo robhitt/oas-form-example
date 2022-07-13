e877c4b87d5ec799d45f16727e612660d1fd7b9a
Rob Hitt <rob.h@taboola.com>
2022-07-13 10:58:34 -0400

    first commit

--- src/components/ExampleForm.js
+++ src/components/ExampleForm.js
@@ -1,41 +1,142 @@
 import React, { useState } from 'react';
 
 function ExampleForm() {
-  const [formData, setFormData] = useState({ fullName: '', email: '' });
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    bio: '',
+    isFriendly: false,
+    favoriteMeal: 'lunch',
+  });
 
+  // destructure
   const handleChange = ({ target }) => {
+    const { name, value, checked, type } = target;
     setFormData((prevState) => ({
       ...prevState,
-      [target.name]: target.value,
+      [name]: type === 'checkbox' ? checked : value,
     }));
-    console.log(formData);
+    // console.table(formData);
   };
 
   return (
     <div>
-      <h3>Example Form</h3>
-      <h4>2022-05-04</h4>
-
-      <p>---------</p>
+      {/* <h3>Ex. Form - Local State / React Hooks</h3>
+      <div>2022-07-13</div> */}
 
       <form>
+        {/* Associate for w/ the id property */}
+        {/* htmlFor as jsx is Javascript and for is a reserved word */}
+        {/* Click Text Inside of a label tag will focus on the input */}
+        <label htmlFor="firstName"></label>
         <input
           type="text"
-          name="fullName"
-          id="fullName"
+          name="firstName"
+          id="firstName"
           onChange={handleChange}
-          placeholder="Placeholder Name"
-          value={formData.fullName}
+          placeholder="First Name"
+          value={formData.firstName}
         />
 
+        <label htmlFor="lastName"></label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          onChange={handleChange}
+          placeholder="Last Name"
+          value={formData.lastName}
+        />
+
+        <label htmlFor="email"></label>
         <input
           type="email"
           name="email"
           id="email"
           onChange={handleChange}
-          placeholder="Placeholder Email"
+          placeholder="Email"
           value={formData.email}
         />
+
+        <label htmlFor="bio"></label>
+        <textarea
+          name="bio"
+          value={formData.bio}
+          onChange={handleChange}
+          placeholder="Biography"
+        />
+
+        <label
+          htmlFor="isFriendly"
+          style={{
+            display: 'inline-block',
+            padding: '8px',
+            borderRadius: '5px',
+          }}
+          className={formData.isFriendly ? 'friendly-border' : ''}
+        >
+          <input
+            type="checkbox"
+            name="isFriendly"
+            id="isFriendly"
+            onChange={handleChange}
+            checked={formData.isFriendly}
+            style={{ border: '2px solid #000' }}
+          />
+          Are you friendly?
+        </label>
+
+        <fieldset style={{ marginTop: '10px' }}>
+          <legend>Favorite Meal</legend>
+          <input
+            type="radio"
+            id="breakfast"
+            name="favoriteMeal"
+            value="breakfast"
+            checked={formData.favoriteMeal === 'breakfast'}
+            onChange={handleChange}
+          />
+          <label htmlFor="breakfast">Breakfast</label>
+          <br />
+          <input
+            type="radio"
+            id="lunch"
+            name="favoriteMeal"
+            value="lunch"
+            checked={formData.favoriteMeal === 'lunch'}
+            onChange={handleChange}
+          />
+          <label htmlFor="lunch">Lunch</label>
+          <br />
+          <input
+            type="radio"
+            id="dinner"
+            name="favoriteMeal"
+            value="dinner"
+            checked={formData.favoriteMeal === 'dinner'}
+            onChange={handleChange}
+          />
+          <label htmlFor="dinner">Dinner</label>
+        </fieldset>
+
+        <br />
+
+        <select
+          id="favColor"
+          value={formData.favColor}
+          onChange={handleChange}
+          name="favColor"
+        >
+          <option value="">-- Choose --</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="indigo">Indigo</option>
+          <option value="violet">Violet</option>
+        </select>
       </form>
     </div>
   );
